bd85d63184556134b1ddb1e1d72f74cc722ca1aa
lilichuan <luffy74123@gmail.com>
2021-06-21 11:33:49 +0800

    Back up

--- app/src/main/java/com/lililib/taiwanadministrativearea/lib/FirstClassArea.java
+++ app/src/main/java/com/lililib/taiwanadministrativearea/lib/FirstClassArea.java
@@ -1,2 +1,15 @@
-package com.lililib.taiwanadministrativearea.lib;public class FirstClassArea {
+package com.lililib.taiwanadministrativearea.lib;
+
+import androidx.annotation.NonNull;
+
+abstract public class FirstClassArea {
+
+    abstract public @NonNull
+    String getChineseName();
+
+    abstract public @NonNull
+    String getEngName();
+
+
+
 }
